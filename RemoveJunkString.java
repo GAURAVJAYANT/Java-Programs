5f2c682440767e9930669766b695d0a3071d1326
Gaurav Jayant <gaurav.jayant@groupbayport.com>
2025-12-08 07:54:14 +0530

    Add another test case to RemoveJunkString

--- RemoveJunkString.java
+++ RemoveJunkString.java
@@ -1,7 +1,10 @@
 public class RemoveJunkString {
     public static void main(String[] args) {
         String s = "$$#$12345&*%";
+        String s1 = "%$%%^&*&*Testing^%$%%Selenium&&&&";
         s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s1 = s1.replaceAll("[^a-zA-Z0-9]", "");
         System.out.println(s);
+        System.out.println(s1);
     }
 }
